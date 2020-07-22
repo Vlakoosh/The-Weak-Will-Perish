9127652a8af0dc76fa2cc1d7684806ddc9db72a5
Vlakoosh <68621923+Vlakoosh@users.noreply.github.com>
2020-07-22 02:50:39 +0200

    Revert "Add files via upload"
    This reverts commit 19a61074dc5f6d711dd2597638325dfd541f93b5.

--- Scenes/signaler.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-signal nextLevel
-
-
-func _on_Next_Room_nextLevel():
-	emit_signal("nextLevel")
--- Stats.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-export(int) var health = 1
-export(int) var damage = 1
